079c3104fce047a302e9ae3004eb4b2135c25497
HiteshChugh-2006 <hiteshchugh101@gmail.com>
2026-03-01 15:07:04 +0530

    Prepare for Render deployment

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const app = require("./src/app");
 const { connectRedis } = require("./src/config/redis");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 async function startServer() {
-    await connectRedis();
-
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
+  await connectRedis();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 }
+
+startServer();
